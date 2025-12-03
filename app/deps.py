f9907d94ee5673989657e5d2958ce24087f5c473
Aryaman Chakraborty <72442257+aryaman911@users.noreply.github.com>
2025-12-03 01:29:39 -0500

    Update import statements for app module structure

--- app/deps.py
+++ app/deps.py
@@ -1,12 +1,11 @@
-
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 
-from .database import get_db
-from .models import ADPUser, ADPRole, ADPUserRole
-from .auth_utils import decode_access_token
-from .schemas import TokenData
+from app.database import get_db
+from app.models import ADPUser
+from app.auth_utils import decode_access_token
+from app.schemas import TokenData
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
